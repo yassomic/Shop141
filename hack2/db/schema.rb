7fd69aca7c44cdd7bd7716cb26f698360b59d03b
MakerSquare Student <students@themakersquare.com>
2014-01-10 15:09:09 -0600

    Added a price column to Product model

--- hack2/db/schema.rb
+++ hack2/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140110205623) do
+ActiveRecord::Schema.define(version: 20140110210033) do
 
   create_table "charities", force: true do |t|
     t.string   "name"
@@ -28,8 +28,10 @@ ActiveRecord::Schema.define(version: 20140110205623) do
     t.text     "img"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.decimal  "price"
   end
 
   add_index "products", ["charity_id"], name: "index_products_on_charity_id"
 
 end
+
