89bab39e75a6494477611dc0d824c468833dcaf4
amhayslip <amhayslip@gmail.com>
2014-01-15 07:59:00 -0600

    Passing test for refactored Product controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,56 +2,107 @@ class Product < ActiveRecord::Base
 
   belongs_to :charity
   
-  after_create :product_query
+  # after_create :product_query
+  after_create :configure_item, :set_title_url_params, :set_price_pic_params, :set_title, :set_price, :set_picture
 
  #method cals the Amazon API
- def product_query
-    req = Vacuum.new 
-    req.configure(
-      aws_access_key_id: ENV['ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['SECRET_KEY'],
-      associate_tag: ENV['ASSOCIATES_ID']
-    )
 
-    req.associate_tag = 'shop1410f-20'
+ def configure_item
+  @req = Vacuum.new 
+      @req.configure(
+        aws_access_key_id: ENV['ACCESS_KEY_ID'],
+        aws_secret_access_key: ENV['SECRET_KEY'],
+        associate_tag: ENV['ASSOCIATES_ID']
+      )
+      @req.associate_tag = 'shop1410f-20'
+      item_id = self.item_id
+ end
 
-    item_id = self.item_id
+ def set_title_url_params
+  @title_url_params = {
+        'IdType'        => 'ASIN',
+        'ItemId'        => item_id,
+      }
+ end
 
-    params = {
-      'IdType'        => 'ASIN',
-      'ItemId'        => item_id,
-      'ResponseGroup' => 'Offers, Images'
-    }
+ def set_title
+  @title_res = @req.item_lookup(@title_url_params)
+  hash = @title_res.to_h
+  self.name = hash["ItemLookupResponse"]["Items"]["Item"]["ItemAttributes"]["Title"]
+  self.save
+ end
 
-  #For Product Price  
-    res = req.item_lookup(params)
+ def set_price_pic_params
+  @price_pic_params = {
+        'IdType'        => 'ASIN',
+        'ItemId'        => item_id,
+        'ResponseGroup' => 'Offers, Images'
+      }
+ end
 
-    hash = res.to_h
-    self.price = hash["ItemLookupResponse"]["Items"]["Item"]["Offers"]["Offer"]["OfferListing"]["Price"]["FormattedPrice"]
+ def set_price
+  @price_res = @req.item_lookup(@price_pic_params)
+  hash = @price_res.to_h
+  self.price = hash["ItemLookupResponse"]["Items"]["Item"]["Offers"]["Offer"]["OfferListing"]["Price"]["FormattedPrice"]
+  self.save
+ end
 
-  #For Product Picture 
-    res = req.item_lookup(params)
-    hash = res.to_h 
-    self.img = hash["ItemLookupResponse"]["Items"]["Item"]["LargeImage"]["URL"]
+ def set_picture
+  @pic_res = @req.item_lookup(@price_pic_params)
+  hash = @pic_res.to_h 
+  self.img = hash["ItemLookupResponse"]["Items"]["Item"]["LargeImage"]["URL"]
+  self.save
+ end
+
+
+
+ # def product_query
+ #    req = Vacuum.new 
+ #    req.configure(
+ #      aws_access_key_id: ENV['ACCESS_KEY_ID'],
+ #      aws_secret_access_key: ENV['SECRET_KEY'],
+ #      associate_tag: ENV['ASSOCIATES_ID']
+ #    )
+
+ #    req.associate_tag = 'shop1410f-20'
+
+ #    item_id = self.item_id
+
+ #    params = {
+ #      'IdType'        => 'ASIN',
+ #      'ItemId'        => item_id,
+ #      'ResponseGroup' => 'Offers, Images'
+ #    }
+
+ #  #For Product Price  
+ #    res = req.item_lookup(params)
+
+ #    hash = res.to_h
+ #    self.price = hash["ItemLookupResponse"]["Items"]["Item"]["Offers"]["Offer"]["OfferListing"]["Price"]["FormattedPrice"]
+
+ #  #For Product Picture 
+ #    res = req.item_lookup(params)
+ #    hash = res.to_h 
+ #    self.img = hash["ItemLookupResponse"]["Items"]["Item"]["LargeImage"]["URL"]
  
 
-    params = {
-      'IdType'        => 'ASIN',
-      'ItemId'        => item_id,
-    }
+ #    params = {
+ #      'IdType'        => 'ASIN',
+ #      'ItemId'        => item_id,
+ #    }
 
-  #For Amazon URL
-    res = req.item_lookup(params)
-    hash = res.to_h
-    self.amazonUrl = hash["ItemLookupResponse"]["Items"]["Item"]["ItemLinks"]["ItemLink"][0]["URL"]
+ #  #For Amazon URL
+ #    res = req.item_lookup(params)
+ #    hash = res.to_h
+ #    self.amazonUrl = hash["ItemLookupResponse"]["Items"]["Item"]["ItemLinks"]["ItemLink"][0]["URL"]
  
 
-  #For Product Title
-    res = req.item_lookup(params)
-    hash = res.to_h
-    self.name = hash["ItemLookupResponse"]["Items"]["Item"]["ItemAttributes"]["Title"]
-    self.save
+ #  #For Product Title
+ #    res = req.item_lookup(params)
+ #    hash = res.to_h
+ #    self.name = hash["ItemLookupResponse"]["Items"]["Item"]["ItemAttributes"]["Title"]
+ #    self.save
 
-  end
+ #  end
 
 end
--- spec/model/product_spec.rb
+++ spec/model/product_spec.rb
@@ -1,6 +1,22 @@
 require 'spec_helper'
 
 describe Product do
+  context 'when product is created' do
+    let(:product) do
+      Product.create \
+        item_id: 'B0077BZ6GI'
+    end
+
+    it 'configures Vacuums call to the API' do
+      expect(product.item_id).to eq 'B0077BZ6GI'
+    end
+
+    it '' do
+    end
+
+  end
+
+
   context 'when given a valid item_id' do
     let(:product) do
       Product.create \
@@ -13,4 +29,5 @@ describe Product do
       expect(product.img).to eq 'http://ecx.images-amazon.com/images/I/41WK5jdfIQL.jpg'
     end
   end
+
 end
