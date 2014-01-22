7e11b29c617c559131a8d1059404d5532becf0e7
amhayslip <amhayslip@gmail.com>
2014-01-22 11:13:04 -0600

    Add fading navbar on scroll

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -9,3 +9,29 @@ jQuery(document).ready(function($){
     ); 
 });
 
+
+$elem1 = $('.index-title-box');
+$elem2 = $('.navhead');
+
+var scrollState = 'top';
+
+$(window).scroll(function(){ 
+    
+    var scrollPos = $(window).scrollTop();
+
+    if( ( scrollPos != 0 ) && ( scrollState === 'top' ) ) {
+        $elem1.stop().animate({marginBottom: '-40px'}, 300);
+        // $elem2.css({ 'background-color': '#E5F9EE' })
+        $elem2.fadeTo("slow", 0.5);
+        scrollState = 'scrolled';
+    }       
+    else if( ( scrollPos === 0 ) && ( scrollState === 'scrolled' ) ) {
+        $elem1.stop().animate({marginBottom: '10px'}, 300);
+        // $elem2.css({ 'background-color': '#E8E9E5' })
+        $elem2.fadeTo("slow", 1);
+        scrollState = 'top';
+    }
+
+});
+
+
