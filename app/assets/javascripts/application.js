b3bc7537f24868d3fa1894bf3b520942527a116d
Michael Yasso <yassomic@gmail.com>
2014-01-21 22:32:30 -0600

    Create a parallax landing page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,22 @@
 //= require jquery_ujs
 //= require_tree .
 
-$(function(){ $(document).foundation(); });
+// $(function(){ $(document).foundation(); });
 
+$(document).ready(function(){
 
+// Parallax
+var $window = $(window);
+
+	$('div[data-type="background"]').each(function(){
+	var $bgobj = $(this); // assigning the object
+
+	$(window).scroll(function() {
+		var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+	// Put together our final background position
+	var coords = '50% '+ yPos + 'px';
+	// Move the background
+	$bgobj.css({ backgroundPosition: coords });
+		});
+	});
+});
