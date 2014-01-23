8463575be036008263f01e0c362f5b37b79f06da
amhayslip <amhayslip@gmail.com>
2014-01-23 13:45:45 -0600

    configure action mailer

--- Gemfile
+++ Gemfile
@@ -62,6 +62,7 @@ gem 'foundation-icons-sass-rails'
 gem 'nokogiri'
 gem 'friendly_id', '~> 5.0.0'
 gem 'devise'
+gem 'devise_invitable'
 
 
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Hack2::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -27,4 +27,14 @@ Hack2::Application.configure do
   # number of complex assets.
   config.assets.debug = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV["EMAIL_ACCOUNT"],
+    :password             => ENV["EMAIL_PASSWORD"],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end
