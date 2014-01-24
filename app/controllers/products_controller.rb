b7de1ee115d90988c14b91fe5a52a0f2a04d27a4
amhayslip <amhayslip@gmail.com>
2014-01-24 15:17:05 -0600

    Add edit and update product methods on viewable by signed in charity

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -25,9 +25,21 @@ class ProductsController < ApplicationController
     redirect_to charity_path(@charity.slug)
   end
 
+  def edit
+    @charity = Charity.friendly.find(params[:charity_id])
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    @charity = Charity.friendly.find(params[:charity_id])
+    redirect_to charity_path(@charity.slug)
+  end
+
   private
   def product_params
-    params.require(:product).permit(:item_id)
+    params.require(:product).permit(:item_id, :description)
   end
 
  
